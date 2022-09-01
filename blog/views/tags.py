c7cda208c64177eacd4eba30962213295e26ddc8
nxNghia <nghia1421999@gmail.com>
2022-09-01 11:38:09 +0700

    redirect from menu

--- blog/views/tags.py
+++ blog/views/tags.py
@@ -12,3 +12,17 @@ def tag_child():
     
 
     return jsonify(tags)
+
+
+@app.route('/tag-child/<int:id>', methods=['GET'])
+def post_by_tag(id):
+    posts = db.session.query(Post, User, Tag).join(Tag, User).filter(Post.tag_id==id).all()
+    point = []
+    userLike = []
+    for post in posts:
+        _point_ = db.session.query(Like).filter(Like.post_id==post['Post'].id).all()
+        point.append(len(_point_))
+        like = db.session.query(Like).filter(Like.post_id == post['Post'].id, Like.user_id == session["logged_in"]['id']).count()
+        userLike.append(like)
+
+    return render_template('post/list-post.html', posts=posts, point=point, length=len(point), userLike = userLike)
